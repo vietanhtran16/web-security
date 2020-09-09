a71001688997c5792bc1fbf138c34b54f4e12a37
Viet Anh Tran <viet.tran@fairfaxmedia.co.nz>
2020-09-10 09:22:11 +1200

    Stop other sites from iframing trusted site with X Frame Options

--- 02-Clickjacking/trusted-site/src/index.js
+++ 02-Clickjacking/trusted-site/src/index.js
@@ -3,8 +3,13 @@ const app = express();
 const path = require('path');
 
 const port = 5000;
-const STATIC_FOLDER = path.join(process.cwd(), "static");
 
+app.use((req, res, next) => {
+  res.setHeader("X-Frame-Options", "DENY");
+  next();
+});
+
+const STATIC_FOLDER = path.join(process.cwd(), "static");
 app.use(express.static(STATIC_FOLDER));
 
 app.listen(port, () => {
