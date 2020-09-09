d58a7b154e265c86c7fbd097db82d02f05aa1a4b
Viet Anh Tran <viet.tran@fairfaxmedia.co.nz>
2020-09-10 09:19:06 +1200

    Overlay button on ifram for clickjacking

--- 02-Clickjacking/malicious-site/src/App.js
+++ 02-Clickjacking/malicious-site/src/App.js
@@ -4,6 +4,10 @@ import Typography from "@material-ui/core/Typography";
 
 const containerStyle = {marginTop: "20px", display: "flex", flexDirection: "column"};
 
+const handleOnClick = (event) => {
+  alert("Coronavirus is being downloaded");
+};
+
 function App() {
   return (
     <>
@@ -16,7 +20,9 @@ function App() {
       </AppBar>
       <Container style={containerStyle} maxWidth="sm">
         <div>Hello</div>
-        <iframe src="http://localhost:5000/" title="Hello"/>
+        <button onClick={handleOnClick}>
+          <iframe src="http://localhost:5000/" title="Hello"/>
+        </button>
       </Container>
     </>
   );
