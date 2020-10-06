84b03b818890c0a429ffdb9010a79c19a587b04d
Viet Anh Tran <viet.tran@fairfaxmedia.co.nz>
2020-10-07 09:55:37 +1300

    Generate random nonce

--- 01-XSS/origin-site/server-src/index.js
+++ 01-XSS/origin-site/server-src/index.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const crypto = require('crypto');
+const { readFile } = require('fs');
 
 const port = 8000;
 const DIST_FOLDER = path.join(process.cwd(), "build");
 
-app.use((req, res, next) => {
+app.use((_, res, next) => {
   res.setHeader("Set-Cookie", "name=Viet")
   next();
 })
 
-app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' 'nonce-1234'; style-src 'self' 'unsafe-inline'");
+app.use((_, res, next) => {
+  const nonce = crypto.randomBytes(16).toString('base64');
+  res.nonce = nonce;
+  res.setHeader("Content-Security-Policy", `default-src 'self'; script-src 'self' 'nonce-${nonce}'; style-src 'self' 'unsafe-inline'`);
   next();
+});
+
+app.get("/", async (_, res) => {
+  readFile(path.join(DIST_FOLDER, "index.html"), (error, indexFile) => {
+    if(error) {
+      res.sendStatus(500);
+    } else {
+      const indexWithUpdatedNonce = indexFile.toString().split("${NONCE}").join(res.nonce);
+      res.send(indexWithUpdatedNonce);
+    }
+  });
 });
 
 app.use(express.static(DIST_FOLDER));
